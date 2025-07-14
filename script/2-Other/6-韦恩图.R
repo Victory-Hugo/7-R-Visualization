0f4c90a4a66d4647a8541204547394e07c59fa84
Victory-Hugo <2023110098@stu.cqmu.edu.cn>
2025-07-14 20:53:20 +0800

    2025年7月14日

--- script/2-Other/6-韦恩图.R
+++ script/2-Other/6-韦恩图.R
@@ -4,7 +4,7 @@ library(venn)         #韦恩图（venn 包，适用样本数 2-7）
 library(VennDiagram) 
 
 # 读取数据文件
-venn_dat <- read.delim('Haplogroup_vene.tsv') # 每一列是一个集合
+venn_dat <- read.delim('/mnt/d/幽门螺旋杆菌/Script/分析结果/2-变异统计/output/venn_counts.csv',sep = ',') # 每一列是一个集合
 venn_list <- list(venn_dat[,1], venn_dat[,2],venn_dat[,3])   # 制作韦恩图搜所需要的列表文件
 names(venn_list) <- colnames(venn_dat[1:3])    # 把列名赋值给列表的key值
 venn_list = purrr::map(venn_list,na.omit)      # 删除列表中每个向量中的NA
