a0e40bf2d4e8da558aa9e953cf722529d390d4db
Victory-Hugo <2023110098@stu.cqmu.edu.cn>
2025-07-15 11:45:01 +0800

    2025年7月15日2025年7月15日

--- script/2-Other/1-统计柱状图饼图调用.sh
+++ script/2-Other/1-统计柱状图饼图调用.sh
@@ -1,7 +1,7 @@
 /home/luolintao/miniconda3/envs/pyg/bin/python3 \
     /mnt/f/OneDrive/文档（科研）/脚本/Download/7-R-Visualization/python/1-统计柱状图饼图.py \
-    -i /mnt/f/OneDrive/文档（科研）/P0_幽门螺旋杆菌/基础信息表/HP数据收集.csv \
-    -s "," \
+    -i /mnt/f/OneDrive/文档（科研）/P0_幽门螺旋杆菌/基础信息表/7544个样本.txt \
+    -s "\t" \
     -n 10 \
-    -c "Country,Continent" \
+    -c "Country,Continent,Chromopainter4" \
     -o "/mnt/f/OneDrive/文档（科研）/脚本/Download/7-R-Visualization/pdf/"
